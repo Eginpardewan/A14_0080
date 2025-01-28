5d94e0b639925bca46a060fcb4367222b6163b73
Egin Pardewan <eginpardewan@gmail.com>
2025-01-28 19:56:28 +0700

    membuat updatetview untuk pelanggan

---
+++ app/src/main/java/com/example/pamuas_080/ui/view/pelanggan/UpdatePelangganView.kt
@@ -0,0 +1,61 @@
+package com.example.pamuas_080.ui.view.pelanggan
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pamuas_080.ui.customwidget.CstTopAppBar
+import com.example.pamuas_080.ui.navigation.DestinasiNavigasi
+import com.example.pamuas_080.ui.navigation.DestinasiUpdatePelanggan
+import com.example.pamuas_080.ui.viewmodel.PenyediaViewModel
+import com.example.pamuas_080.ui.viewmodel.pelanggan.UpdatePelangganViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateScreen(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate:()-> Unit,
+    viewModel: UpdatePelangganViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CstTopAppBar(
+                title = DestinasiUpdatePelanggan.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = onBack,
+            )
+        }
+    ){padding ->
+        EntryBody(
+            modifier = Modifier.padding(padding),
+            insertUiState = viewModel.updatePelangganUiState,
+            onPelangganValueChange = viewModel::updateInsertPelangganState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updatePelanggan()
+                    delay(600)
+                    withContext(Dispatchers.Main){
+                        onNavigate()
+                    }
+                }
+            }
+        )
+    }
+}
